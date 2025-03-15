297ff732ed4353830997ad25c6d792ba4ad5d941
jimjflannery <jimjflannery@gmail.com>
2025-03-15 17:25:22 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,6 +13,6 @@ self.addEventListener('install', e => {
 });
 self.addEventListener('fetch', e => {
     e.respondWith(
-        caches. Match(e.request).then(res => res || fetch(e.request))
+        caches.match(e.request).then(res => res || fetch(e.request))
     );
 });
